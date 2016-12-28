9d9968e5c9590dafcf1845f0faa7fa5f5057f563
bartlomiejpogorzelski <bartlomiej.pogorzelski@onet.pl>
2016-12-28 12:15:23 +0100

    Add class Employee and "<code>"

---
+++ src/coursejava/documentation/Employee.java
@@ -0,0 +1,10 @@
+package coursejava.documentation;
+
+/**
+ * Class <code>EMployee</code> represents employees and its super class for ... 
+ * @author Bartłomiej Pogorzelski
+ * @version 1.2.5. 28/12/2016
+ */
+public class Employee {
+    
+}
